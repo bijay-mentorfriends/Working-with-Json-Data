0af12471ae8d3035b7a9e6784e6bf1137510a761
bijay-mentorfriends <bijayjoshi@mentorfriends.com>
2023-03-15 16:29:33 +0545

    converted

--- FreeSchema1.1/Controllers/WeatherForecastController.cs
+++ FreeSchema1.1/Controllers/WeatherForecastController.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Json;
 using System.Text.Json;
 using System.Text.Json.Nodes;
+using System.Xml.Linq;
 
 namespace FreeSchema1._1.Controllers
 {
@@ -11,11 +16,7 @@ namespace FreeSchema1._1.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
+    
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -24,101 +25,224 @@ namespace FreeSchema1._1.Controllers
         }
 
         [HttpPost("GetData")]
-        public Dictionary<string, dynamic> GetData([FromBody] dynamic forecast) {
+        public void GetData([FromBody] dynamic forecast) {
 
-            Dictionary<string, dynamic> initialData = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(forecast.GetRawText());
+            Console.WriteLine(forecast.GetType());
+
+            JsonElement d = forecast as dynamic;
+            JsonValueKind jsonValueKind = d.ValueKind;
+            Dictionary<string, dynamic> initialData = new Dictionary<string, dynamic>();
+
+            if (jsonValueKind == JsonValueKind.Array)
+            {
+                Console.WriteLine("Ok");
+
+                var list = JsonConvert.DeserializeObject<IEnumerable<KeyValuePair<string, string>>>(forecast.GetRawText());
+
+                Console.WriteLine("Please entery JSOn data instead of array");
+            }
+            else
+            {
+                // Console.WriteLine(JsonConvert.SerializeObject(finalData));
+               initialData = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(forecast.GetRawText());
+
+                dynamic test = dataLooping(initialData);
+                Console.WriteLine(JsonConvert.SerializeObject(test));
+            }
+
+            
+           
+           
+          
+        }
+
+        private Dictionary<string, dynamic> dataLooping(Dictionary<string, dynamic> initialData)
+        {
+
+            Dictionary<string, dynamic> noObjects = new();
+            Dictionary<string, dynamic> objects = new();
 
             Dictionary<string, dynamic> finalData = new Dictionary<string, dynamic>();
 
             foreach (KeyValuePair<string, dynamic> kvp in initialData)
             {
-                dynamic data;
-                Console.WriteLine(kvp.Value.GetType());
+                dynamic ValueData = kvp.Value;
 
-                if (kvp.Value.GetType() == typeof(JArray))
+
+                if (ValueData.GetType() == typeof(JArray) )
                 {
                     var i = 0;
-                    Dictionary<string, dynamic> temp = new Dictionary<string, dynamic>();
-                    foreach (dynamic item in kvp.Value)
+
+                   List<dynamic> temp = new List<dynamic>();
+                    Dictionary<string, dynamic> x = new Dictionary<string, dynamic>();
+                    List<Dictionary<string, dynamic>> abc = new List<Dictionary<string, dynamic>>();
+
+                    foreach (dynamic item in ValueData)
                     {
-                        temp.Add( i.ToString(), item);
+                        if (item.GetType() == typeof(JObject))
+                        {
+                            Dictionary<string, dynamic> dItem = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(item.ToString());
+                            abc.Add(dataLooping(dItem));
+                        }
+                        else
+                        {
+                            temp.Add(item);
+                        }
+                
+
+                      /*  if (item.GetType() == typeof(JObject))
+                        {
+                            temp.Add(i.ToString(), dataLooping(dItem));
+                        }
+                        else
+                        {
+                            temp.Add(i.ToString(), item);
+                        }*/
+                      
                         i++;
                     }
-                 
-                    data = JsonConvert.SerializeObject(temp);
 
-                }
-                else if (kvp.Value.GetType() == typeof(JObject))
+                    if (abc.Count > 0)
+                    {
+                         x.Add(kvp.Key, DicListToDictionary(abc));
+                    }
+                    else
+                    {
+                          x.Add(kvp.Key, StringListToDictionary(temp));
+                    }
+
+                    
+
+                    objects = mergeDictionary(objects, x);
+
+                    // data = JsonConvert.SerializeObject(temp);
+
+                } else if (ValueData.GetType() == typeof(JObject))
                 {
-                    Console.WriteLine("Object Found");
+                    Dictionary<string, dynamic> x = new Dictionary<string, dynamic>();
+                    Dictionary<string, dynamic> dData = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(ValueData.ToString());
+                    Console.WriteLine(dData);
+                   
+                    Dictionary<string, dynamic> y = dataLooping(dData);
+                    x.Add(kvp.Key, y);
 
-                    data = GetData(kvp.Value);
-                    Console.WriteLine(data);
+                    objects = mergeDictionary(objects, x);
+
+                    Console.WriteLine(objects);
                 }
                 else
                 {
-                    data = kvp.Value;
+                    noObjects.Add(kvp.Key, ValueData);
                 }
 
                 // Console.WriteLine(data);
+             }
 
-                finalData.Add(kvp.Key, data);
-            }
+            finalData = mergeDictionary(objects, noObjects);
 
             return finalData;
 
-            //     Console.WriteLine(JsonConvert.SerializeObject(finalData));
-
         }
 
-      /*  private void getChild(dynamic data)
+        private Dictionary<string, dynamic> DicListToDictionary(List<Dictionary<string, dynamic>> myList)
         {
+            int count = 0;
+            Dictionary<string, dynamic> myDict = new Dictionary<string, dynamic>();
 
-            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(data.GetRawText());
-            //Console.WriteLine(dictionary);
-
-            Dictionary<string, dynamic> finalData = new Dictionary<string, dynamic>();
-
-
-            //Console.WriteLine(item);
-            foreach (KeyValuePair<string, object> kvp in dictionary)
+            foreach (Dictionary<string, dynamic> myString in myList)
             {
-
-                dynamic json = kvp.Value;
-
-                if (json.GetType() == typeof(JArray))
-                {
-                    // Console.WriteLine(json);
-                    var i = 0;
-                    Dictionary<string, dynamic> temp = new Dictionary<string, dynamic>();
-                    foreach (JObject item in json)
-                    {
-
-                        temp.Add(i.ToString(), item);
-
-                        i++;
-                    }
-
-                    data = JsonConvert.SerializeObject(temp);
-
-                    foreach (KeyValuePair<string, object> kvp_data in data)
-                    {
-                        if (kvp_data.GetType() == typeof(JArray))
-                        {
-                            getChild(kvp_data.Value);
-                        }
-                        
-                    }
-
-
-                        
-                }
-                
-
-          
+                myDict.Add(count.ToString(), myString);
+                count++;
             }
 
+            return myDict;
+        }
 
-        }*/
+        private Dictionary<string, dynamic> StringListToDictionary(List<dynamic> myList)
+        {
+            int count = 0;
+            Dictionary<string, dynamic> myDict = new Dictionary<string, dynamic>();
+
+            foreach (dynamic myString in myList)
+            {
+                myDict.Add(count.ToString(), myString);
+                count++;
+            }
+
+            return myDict;
+        }
+
+        private Dictionary<string, dynamic> listToDictionary(List<dynamic> myList)
+        {
+            int count = 0;
+            Dictionary<string, dynamic> myDict = new Dictionary<string, dynamic>();
+
+            foreach (string myString in myList)
+            {
+                myDict.Add(count.ToString(), myString);
+                count++;
+            }
+
+            return myDict;
+        }
+
+        private Dictionary<string, dynamic> mergeDictionary(Dictionary<string, dynamic> d1, Dictionary<string, dynamic> d2)
+        {
+            foreach (KeyValuePair<string, dynamic> kvp in d1)
+            {
+                d2.Add(kvp.Key, kvp.Value);
+            }
+            return d2;
+        }
+
+        /*  private void getChild(dynamic data)
+          {
+
+              var dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(data.GetRawText());
+              //Console.WriteLine(dictionary);
+
+              Dictionary<string, dynamic> finalData = new Dictionary<string, dynamic>();
+
+
+              //Console.WriteLine(item);
+              foreach (KeyValuePair<string, object> kvp in dictionary)
+              {
+
+                  dynamic json = kvp.Value;
+
+                  if (json.GetType() == typeof(JArray))
+                  {
+                      // Console.WriteLine(json);
+                      var i = 0;
+                      Dictionary<string, dynamic> temp = new Dictionary<string, dynamic>();
+                      foreach (JObject item in json)
+                      {
+
+                          temp.Add(i.ToString(), item);
+
+                          i++;
+                      }
+
+                      data = JsonConvert.SerializeObject(temp);
+
+                      foreach (KeyValuePair<string, object> kvp_data in data)
+                      {
+                          if (kvp_data.GetType() == typeof(JArray))
+                          {
+                              getChild(kvp_data.Value);
+                          }
+
+                      }
+
+
+
+                  }
+
+
+
+              }
+
+
+          }*/
     }
 }
